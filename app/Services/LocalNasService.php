fce4de6688ae01869c571fab3e0d541e5c0cd101
Ilhammeru <gumilang.dev@gmail.com>
2024-05-27 12:51:17 +0700

    loggin

--- app/Services/LocalNasService.php
+++ app/Services/LocalNasService.php
@@ -129,7 +129,7 @@ class LocalNasService {
             $datetime = date('YmdHis');
             $name = "uploaded_file_{$datetime}.{$ext}";
 
-            $mainAddon = Storage::putFileAs('addons', $file, $name);
+            $mainAddon = Storage::putFile('addons', $file);
             Log::debug('main addon upload res: ', [$mainAddon]);
     
             $path = storage_path('app/public/addons/' . $name);
