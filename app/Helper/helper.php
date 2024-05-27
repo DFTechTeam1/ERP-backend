3b306ae5ba4c36e39aac6b7814cbc1bed070b5aa
Ilhammeru <gumilang.dev@gmail.com>
2024-05-28 04:03:10 +0700

    update nas service

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -334,3 +334,29 @@ if (!function_exists('getCache')) {
         return Cache::get($cacheId);
     }
 }
+
+if (!function_exists('curlRequest')) {
+    function curlRequest(string $url, array $payload)
+    {
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => ['Access-Control-Allow-Origin' => '*',],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_POSTFIELDS => $payload,
+        );
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($response, true);
+    }
+}
