98b4a299487f42fc4bfa248ab90a933fca9e6544
Ilhammeru <gumilang.dev@gmail.com>
2024-06-20 02:48:52 +0700

    update task log

--- Modules/Production/app/Models/ProjectTaskLog.php
+++ Modules/Production/app/Models/ProjectTaskLog.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Production\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -22,8 +23,17 @@ class ProjectTaskLog extends Model
         'user_id',
     ];
 
+    protected $appends = ['time_created'];
+
     public function task(): BelongsTo
     {
         return $this->belongsTo(\Modules\Production\Models\ProjectTask::class, 'project_task_id');
+    }
+
+    public function timeCreated(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => date('d F Y', strtotime($this->attributes['created_at'])) . ' at ' . date('H:i', strtotime($this->attributes['created_at']))
+        );
     }
 }
