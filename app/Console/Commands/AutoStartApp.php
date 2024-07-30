666b6128dc0f5afd77d88555e86018be1de1a80c
Ilhammeru <gumilang.dev@gmail.com>
2024-07-30 10:00:59 +0700

    add installation in command line

--- app/Console/Commands/AutoStartApp.php
+++ app/Console/Commands/AutoStartApp.php
@@ -80,4 +80,67 @@ class AutoStartApp extends Command
     {
         $this->assignEmployeeAsUser();
     }
+
+    protected function assignEmployeeAsUser()
+    {
+        $employees = \Modules\Hrd\Models\Employee::where('status', '!=', \App\Enums\Employee\Status::Inactive->value)
+            ->get();
+
+        \App\MOdels\User::where('email', '!=', 'admin@admin.com')
+            ->delete();
+
+        $directorPosition = json_decode(getSettingByKey('position_as_directors'), true);
+        $directorPosition = collect($directorPosition)->map(function ($item) {
+            return getIdFromUid($item, new \Modules\Company\Models\Position());
+        })->toArray();
+
+        $pmPosition = json_decode(getSettingByKey('position_as_project_manager'), true);
+        $pmPosition = collect($pmPosition)->map(function ($item) {
+            return getIdFromUid($item, new \Modules\Company\Models\Position());
+        })->toArray();
+
+        $marketingPosition = getSettingByKey('position_as_marketing');
+        $marketingPosition = getIdFromUid($marketingPosition, new \Modules\Company\Models\Position());
+
+        $projectManagerRole = Role::findByName('project manager');
+        $productionRole = Role::findByName('production');
+        $directorRole = Role::findByName('director');
+        $marketingRole = Role::findByName('marketing');
+        $suRole = Role::findByName('root');
+
+        foreach ($employees as $employee) {
+            $payload = [
+                'employee_id' => $employee->id,
+                'email' => $employee->email,
+                'email_verified_at' => date('Y-m-d H:i:s'),
+                'password' => \Illuminate\Support\Facades\Hash::make('password'),
+            ];
+
+            if (in_array($employee->position_id, $directorPosition)) {
+                $payload['is_director'] = true;
+                $payload['role'] = 'director';
+            } else if (in_array($employee->position_id, $pmPosition)) {
+                $payload['is_project_manager'] = true;
+                $payload['role'] = 'project manager';
+            } else if ($employee->position_id == $marketingPosition) {
+                $payload['is_employee'] = true;
+                $payload['role'] = 'marketing';
+            } else {
+                $payload['is_employee'] = true;
+                $payload['role'] = 'production';
+            }
+
+            $user = \App\Models\User::create(collect($payload)->except(['role'])->toArray());
+
+            if ($payload['role'] == 'production') {
+                $user->assignRole($productionRole);
+            } else if ($payload['role'] == 'project manager') {
+                $user->assignRole($projectManagerRole);
+            } else if ($payload['role'] == 'director') {
+                $user->assignRole($directorRole);
+            } else if ($payload['role'] == 'marketing') {
+                $user->assignRole($marketingRole);
+            }
+        }
+    }
 }
