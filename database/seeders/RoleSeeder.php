d350c1a9a5d8387b88c3c65df7d630bf35e64941
Ilhammeru <gumilang.dev@gmail.com>
2024-07-31 17:11:21 +0700

    fix region seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -16,6 +17,8 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Schema::disableForeignKeyConstraints();
 
         DB::table('roles')->truncate();
 
@@ -56,6 +59,8 @@ class RoleSeeder extends Seeder
             }
 
         }
+
+        Schema::enableForeignKeyConstraints();
 
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
     }
