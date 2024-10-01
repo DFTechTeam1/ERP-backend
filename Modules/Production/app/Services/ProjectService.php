ffeb362b002fc2650d41e2f7520c92727cdcceed
Ilhammeru <gumilang.dev@gmail.com>
2024-10-01 13:27:25 +0700

    fix minor query bugs in project detail

--- Modules/Production/app/Services/ProjectService.php
+++ Modules/Production/app/Services/ProjectService.php
@@ -774,12 +774,14 @@ class ProjectService
         foreach ($references as $key => $reference) {
             if ($reference->type == 'link') {
                 $group['link'][] = [
-                    'media_path' => $reference->media_path,
+                    'media_path' => 'link',
+                    'link' => $reference->media_path,
                     'id' => $reference->id,
                 ];
             } else if (in_array($reference->type, $fileDocumentType)) {
                 $group['pdf'][] = [
                     'id' => $reference->id,
+                    'name' => 'document',
                     'media_path' => asset('storage/projects/references/' . $projectId) . '/' . $reference->media_path,
                     'type' => $reference->type,
                 ];
