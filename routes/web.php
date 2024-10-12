87e787d92105cde158bc7163f60bac3a1ee52545
Ilhammeru <gumilang.dev@gmail.com>
2024-10-12 10:53:39 +0700

    update cron job

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Enums\Production\ProjectStatus;
+use App\Enums\Production\TaskStatus;
+use App\Jobs\PostNotifyCompleteProjectJob;
+use App\Jobs\UpcomingDeadlineTaskJob;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
+use Modules\Production\Models\ProjectTask;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,11 +15,36 @@ Route::get('/', function () {
 Route::get('generate-official-email', [\App\Http\Controllers\Api\TestingController::class, 'generateOfficialEmail']);
 
 Route::get('ilham', function () {
-    $service = new \App\Services\Geocoding();
+    $endDate = date('Y-m-d', strtotime('+2 days'));
 
-    $response = $service->getCoordinate('Kota Surabaya, Jawa Timur');
+    $tasks = ProjectTask::selectRaw('id,uid,project_id,name')
+        ->with([
+            'pics:id,project_task_id,employee_id',
+            'pics.employee:id,nickname,email,line_id',
+            'project:id,name'
+        ])
+        ->whereIn(
+            'status',
+            [
+                TaskStatus::WaitingApproval->value,
+                TaskStatus::OnProgress->value,
+                TaskStatus::Revise->value,
+            ]
+        )
+        ->where('end_date', $endDate)
+        ->get();
 
-    return $response;
+    $outputData = [];
+    foreach ($tasks as $task) {
+        foreach ($task->pics as $employee) {
+            $outputData[] = [
+                'employee' => $employee,
+                'task' => $task,
+            ];
+        }
+    }
+
+    UpcomingDeadlineTaskJob::dispatch($outputData);
 });
 
 Route::get('login', function () {
