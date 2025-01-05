98338618e9ffae629c802b183e7630af1d54eb0c
ilhammeru <gumilang.dev@gmail.com>
2025-01-05 11:45:00 +0700

    update talenta integration

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Modules\Hrd\Services\TalentaService;
 use Modules\Production\Services\ProjectRepositoryGroup;
 use Vinkla\Hashids\Facades\Hashids;
 
@@ -19,6 +20,16 @@ class LandingPageController extends Controller
 
     public function index()
     {
+        $service = new TalentaService();
+        $service->setUrl(type: 'detail_employee');
+        $service->setUrlParams(params: [
+            'email' => 'email'
+        ]);
+        $response = $service->makeRequest();
+
+        return $response;   
+        if (isset($response['data'])) {
+        }
         return view('landing');
     }
 }
