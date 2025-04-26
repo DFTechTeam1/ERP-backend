06b791a8b1f280c256cbffe7f3338eb3c8c7c5c4
ilhammeru <gumilang.dev@gmail.com>
2025-04-26 13:21:51 +0700

    Fix caching when update the task deadline

--- Modules/Production/app/Services/ProjectService.php
+++ Modules/Production/app/Services/ProjectService.php
@@ -3576,14 +3576,9 @@ class ProjectService
 
             $task = $this->formattedDetailTask($data['task_id']);
 
-            $cache = $this->getDetailProjectCache($projectUid);
-            $currentData = $cache['cache'];
-            $projectId = $cache['projectId'];
-
-            $boards = $this->formattedBoards($projectUid);
-            $currentData['boards'] = $boards;
-
-            storeCache('detailProject' . $projectId, $currentData);
+            $currentData = $this->detailCacheAction->handle($projectUid, [
+                'boards' => FormatBoards::run($projectUid)
+            ]);
 
             DB::commit();
 
