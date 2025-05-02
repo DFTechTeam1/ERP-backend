c9087d23c34e19d87d094c96b9e5c2e749fa3b5a
ilhammeru <gumilang.dev@gmail.com>
2025-05-02 18:37:08 +0700

    remove logging in the landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,34 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\Project\DetailProject;
-use App\Actions\Project\GetProjectStatistic;
-use App\Enums\Employee\Religion;
-
-use App\Enums\Employee\Status;
-use App\Enums\Production\WorkType;
-use App\Exports\NewTemplatePerformanceReportExport;
-use App\Exports\PrepareEmployeeMigration;
-use App\Models\Menu;
-use Carbon\Carbon;
-use Illuminate\Database\Query\JoinClause;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
-use Maatwebsite\Excel\Facades\Excel;
-use Modules\Company\Models\Position;
-use Modules\Company\Models\PositionBackup;
-use Modules\Hrd\Models\Employee;
-use Modules\Hrd\Repository\EmployeePointProjectDetailRepository;
-use Modules\Hrd\Repository\EmployeePointProjectRepository;
-use Modules\Hrd\Repository\EmployeePointRepository;
 use Modules\Hrd\Repository\EmployeeRepository;
 use Modules\Hrd\Services\EmployeePointService;
 use Modules\Hrd\Services\PerformanceReportService;
 use Modules\Production\Services\ProjectRepositoryGroup;
-use \Illuminate\Support\Str;
 
 class LandingPageController extends Controller
 {
@@ -59,8 +35,6 @@ class LandingPageController extends Controller
 
     public function index()
     {
-        $data = $this->employeePointService->renderEachEmployeePoint(28, '2024-12-01', '2024-12-31');
-        return $data;
         // $employees = array(
         //     array('id' => '1','name' => 'Wesley Wiyadi','position_id' => '1'),
         //     array('id' => '2','name' => 'Edwin Chandra Wijaya Ngo','position_id' => '2'),
